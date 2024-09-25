6d34acbe2d64ecbbd81fd72fd5224ec6c9634cf3
Emily Tuttle <emilytuttle00@gmail.com>
2024-09-24 20:52:41 -0600

    arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,8 +15,22 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // MY PROCESS:
+        // The function needs to be given a number to find multiples for (x), and a number to represent the number of multiples to generate (y). This can be done with a loop, that goes until i reaches y. When the loop runs, the current i will be multiplied by x to find the number of multiples. Those multiples will be added to a list, as long as i, which starts at 1, is less than or equal to y. Once all of the multiples have been added to a list, the list is converted into the returned array.
+
+        List<double> multiples = new List<double>();
+
+        for (double i = 1; i <= length; i++)
+        {
+            double currentNumber = number * i;
+            multiples.Add(currentNumber);
+        }
+
+        double[] array = multiples.ToArray();
+
+        return array; // replace this return statement with your own
     }
+
 
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
@@ -29,5 +45,16 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // MY PROCESS
+
+        // To rotate to the right, the last number needs to move to the beginning of the list. This can be done by removing the last number at placing it at the first index. A for loop should be made, with i starting at 0, increasing by one with each run, until the amount of moves to the right have been reached. Within the loop, the last index needs to be found, and this can be done by finding the amount of items in the list and subracting one, as the list starts with an index of 0. Using that index, the current last digit can be stored as a new variable. The last index is then removed, and the value of the last index at the beginning of the loop will be added to the first index of the list. The list is then used again, with the new values and numbers can continue to shift until the amount of rotations has been reached.
+        for (int i = 0; i < amount; i++)
+        {
+            int dataEndIndex = data.Count - 1;
+            int lastIndexValue = data[dataEndIndex];
+            data.RemoveAt(dataEndIndex);
+            data.Insert(0, lastIndexValue);
+        }
     }
 }
